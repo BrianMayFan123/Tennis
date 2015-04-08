1ae4aa017b69356149a3d1defa2c943c06e7ba2a
Conner Evans <connerevans@conners-mbp.home>
2015-04-08 21:39:06 +0100

    Initial Commit

---
+++ Tennis/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Tennis
+//
+
+//  Copyright (c) 2015 CocoBongo Games. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ Tennis/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Tennis
+//
+//  Created by Conner Evans on 08/04/2015.
+//  Copyright (c) 2015 CocoBongo Games. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
